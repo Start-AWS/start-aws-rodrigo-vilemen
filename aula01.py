129b00475bf3d89ca4e51a98b8ba8b99751993a0
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2026-02-26 09:40:30 -0300

    Add variable manipulation example with comments

---
+++ aula01.py
@@ -0,0 +1,7 @@
+# Manipulação de Variáveis e constantes
+
+# - Por meio de variáveis que um algoritmo guardan os dados
+# - Todo dado tem a possivilidade de ser alterado no decorrer do tempo deverá ser tratado como uma variável.
+
+a = 10
+print(a)
