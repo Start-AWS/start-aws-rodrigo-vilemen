b3837369694417eca0d5f22b71c905fc7307e411
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2026-02-27 09:47:29 -0300

    Fix indentation and update invalid option message in age classification; add calculator functionality for addition and subtraction

--- aula03.py
+++ aula03.py
@@ -1,27 +1,11 @@
-# num1 = float(input("Digite o primeiro número: "))
-# num2 = float(input("Digite o segundo número: "))
 
-# print("Escolha a operação:")
-# print("1 - Soma")
-# print("2 - Subtração")
-
-# operacao = input("Digite 1 ou 2: ").strip()
-
-# if operacao == "1":
-#     resultado = num1 + num2
-#     print(f"Resultado da soma: {resultado}")
-# elif operacao == "2":
-#     resultado = num1 - num2
-#     print(f"Resultado da subtração: {resultado}")
-# else:
-#     print("Opção inválida, reiniciando programa.")
 
 idade = int(input("Digite sua idade: "))
 if idade < 0:
-        print("Idade inválida.")
+    print("Idade inválida.")
 elif idade <= 12:
-        print("Criança")
+    print("Criança")
 elif idade <= 17:
-        print("Adolescente")
+    print("Adolescente")
 else:
-        print("Adulto")
+    print("Adulto")
