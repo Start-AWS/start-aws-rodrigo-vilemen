be66195f2d57cb82495ffa5daa79f710e41d1c60
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2026-02-27 09:33:23 -0300

    Refactor variable manipulation examples and add missing newline at end of file

--- aula01.py
+++ aula01.py
@@ -2,6 +2,11 @@
 
 # - Por meio de variáveis que um algoritmo guardan os dados
 # - Todo dado tem a possivilidade de ser alterado no decorrer do tempo deverá ser tratado como uma variável.
+# - Quando um dado não tem possibilidade de variar no decorrer do tempo, deverá ser tratado como constante.
 
 a = 10
 print(a)
+
+x = 10
+x = x + 1
+print(x)
