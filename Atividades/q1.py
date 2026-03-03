b904ea113c278fc9de56d31e20521ac216ce2466
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2026-03-02 23:17:56 -0300

    add codes

--- Atividades/q1.py
+++ Atividades/q1.py
@@ -1,4 +1,6 @@
 # Par ou Ímpar: Leia um número inteiro e informe se ele é par ou ímpar.
+# import os
+# import sys
 
 num = int(input("Digite um número:"))
 
@@ -6,3 +8,24 @@ if num % 2 == 0:
     print("O {num} é par!")
 else:
     print("o {num} é ímpar!")
+
+
+
+
+
+
+
+
+
+
+# opcao = input("Deseja reiniciar o programa? (s/n):")
+
+# if opcao.lower() == "s":
+#     os.execv(sys.executable,
+# [sys.executable] + sys.argv)
+
+# print("fim do programa")
+
+# - sys.executable pega o caminho do Python que está rodando o Script
+# - sys.argv mantém os mesmos argumentos do terminal
+# - os.execv substitui o processo atual por um novo, como se tivesse digitado novamente no terminal
